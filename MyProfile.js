437919c22c9fbdd90dfe663663a37149137d7f18
Isis <isisthechick@gmail.com>
2024-03-01 08:56:38 -0600

    Home page with posts // starting my profile

---
+++ MyProfile.js
@@ -0,0 +1,17 @@
+const openModalBtn = document.getElementById("open-edit");
+const modal = document.getElementById("modal");
+const closeModalBtn = document.getElementsByClassName("close")[0];
+
+openModalBtn.onclick = function() {
+  modal.style.display = "block";
+}
+
+closeModalBtn.onclick = function() {
+  modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
