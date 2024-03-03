e40c34440b60c40db7d80fd6c3f132e8b6ac83e1
Isis <isisthechick@gmail.com>
2024-03-02 21:27:15 -0600

    fixed advanced search on my profile

--- MyProfile.js
+++ MyProfile.js
@@ -1,17 +1,42 @@
 const openModalBtn = document.getElementById("open-edit");
 const modal = document.getElementById("modal");
-const closeModalBtn = document.getElementsByClassName("close")[0];
+const closeModalBtn = document.getElementsByClassName("close")[1];
 
-openModalBtn.onclick = function() {
+openModalBtn.onclick = function(event) {
+  event.preventDefault();
   modal.style.display = "block";
 }
 
-closeModalBtn.onclick = function() {
+closeModalBtn.onclick = function(event) {
+  event.preventDefault();
   modal.style.display = "none";
 }
 
 window.onclick = function(event) {
   if (event.target == modal) {
     modal.style.display = "none";
+  }
+}
+
+
+/* ------ MODAL WINDOW FOR ADVANCE SEARCH ----- */
+
+const openModalBtnSearch = document.getElementById("openModalSearch");
+const modalSearch = document.getElementById("modalSearch");
+const closeModalBtnSearch = document.getElementsByClassName("close")[0];
+
+openModalBtnSearch.onclick = function(event) {
+  event.preventDefault();
+  modalSearch.style.display = "block";
+}
+
+closeModalBtnSearch.onclick = function(event) {
+  event.preventDefault();
+  modalSearch.style.display = "none";
+}
+
+window.onclick = function(event) {
+  if (event.target == modalSearch) {
+    modalSearch.style.display = "none";
   }
 }
