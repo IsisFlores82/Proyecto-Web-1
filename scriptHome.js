c64cd97104292b409dd056629fe9d8ac1f1a4cfc
Isis <isisthechick@gmail.com>
2024-03-02 21:06:02 -0600

    Advanced search and modal windows donee

--- scriptHome.js
+++ scriptHome.js
@@ -1,17 +1,44 @@
 const openModalBtn = document.getElementById("openModal");
 const modal = document.getElementById("modal");
-const closeModalBtn = document.getElementsByClassName("close")[0];
+const closeModalBtn = document.getElementsByClassName("close")[1];
 
-openModalBtn.onclick = function() {
+
+
+openModalBtn.onclick = function(event) {
+  event.preventDefault();
   modal.style.display = "block";
 }
 
-closeModalBtn.onclick = function() {
+closeModalBtn.onclick = function(event) {
+  event.preventDefault();
   modal.style.display = "none";
 }
 
 window.onclick = function(event) {
   if (event.target == modal) {
     modal.style.display = "none";
+  }
+}
+
+
+/* ------ MODAL WINDOW FOR ADVANCE SEARCH ----- */
+
+const openModalBtnSearch = document.getElementById("openModalSearch");
+const modalSearch = document.getElementById("modalSearch");
+const closeModalBtnSearch = document.getElementsByClassName("close")[0];
+
+openModalBtnSearch.onclick = function(event) {
+  event.preventDefault();
+  modalSearch.style.display = "block";
+}
+
+closeModalBtnSearch.onclick = function(event) {
+  event.preventDefault();
+  modalSearch.style.display = "none";
+}
+
+window.onclick = function(event) {
+  if (event.target == modalSearch) {
+    modalSearch.style.display = "none";
   }
 }
